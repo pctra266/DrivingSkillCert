5fdde3bf1781acb78d9d515b479663ab99ebbc7e
LilyStoner <hainam25072004@gmail.com>
2025-03-26 22:11:14 +0700

    comment dai qua thi xuong dong, cau hoi dai cung xuong dong

--- DrivingSkillCert/TeacherRegistrationManagement.xaml.cs
+++ DrivingSkillCert/TeacherRegistrationManagement.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Model.Models;
+﻿using DrivingSkillCert.DAO;
+using Model.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,8 +25,12 @@ namespace DrivingSkillCert
     public partial class TeacherRegistrationManagement : Page
     {
         private DrivingSkillCertContext _context = new DrivingSkillCertContext();
+        private RegistrationDAO _registrationDAO;
+        private User _currentTeacher;
+        private Registration _reg;
         public TeacherRegistrationManagement()
         {
+            _registrationDAO = new RegistrationDAO();
             InitializeComponent();
             LoadRegistrations();
         }
@@ -37,28 +42,14 @@ namespace DrivingSkillCert
                 MessageBox.Show("Bạn chưa đăng nhập. Vui lòng đăng nhập lại.");
                 return;
             }
-            User currentTeacher = currentUserObj as User;
-            if (currentTeacher == null)
+            _currentTeacher = currentUserObj as User;
+            if (_currentTeacher == null)
             {
                 MessageBox.Show("Lỗi dữ liệu người dùng.");
                 return;
             }
 
-            var registrations = (from reg in _context.Registrations
-                                 join c in _context.Courses on reg.CourseId equals c.CourseId
-                                 join student in _context.Users on reg.UserId equals student.UserId
-                                 where c.TeacherId == currentTeacher.UserId
-                                       && reg.IsDelete == false
-                                       && c.IsDelete == false
-                                 select new
-                                 {
-                                     reg.RegistrationId,
-                                     StudentName = student.FullName,
-                                     student.Email,
-                                     CourseName = c.CourseName,
-                                     reg.Status,
-                                     reg.Comments
-                                 }).ToList();
+            var registrations = _registrationDAO.GetRegistrationsOfTeacher(_currentTeacher.UserId);
 
             RegistrationsDataGrid.ItemsSource = registrations;
         }
@@ -144,6 +135,52 @@ namespace DrivingSkillCert
                 mainWindow.Show();
                 Window.GetWindow(this).Close();
             }
+        }
+        private void txtFilter_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            RegistrationsDataGrid.Items.Filter = FilterMethod;
+        }
+
+        private bool FilterMethod(object obj)
+        {
+            var reg = (Registration)obj;
+            return reg.User.FullName.Contains(txtFilter.Text, StringComparison.OrdinalIgnoreCase)
+                || reg.Course.CourseName.Contains(txtFilter.Text, StringComparison.OrdinalIgnoreCase);
+        }
+
+        private void Status_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (_registrationDAO != null && cbStatus.SelectedItem is ComboBoxItem cbItem &&_currentTeacher!=null)
+                RegistrationsDataGrid.ItemsSource = _registrationDAO.GetRegistrationsOfTeacher(_currentTeacher.UserId).Where(e => e.Status.Contains(cbItem.Content.ToString()));
+        }
+
+        private void RegistrationsDataGrid_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if(RegistrationsDataGrid.SelectedItem!=null)
+            {
+                detailPanel.Visibility = Visibility.Visible;  
+                _reg = (Registration)RegistrationsDataGrid.SelectedItem;
+                txtComment.Text = _reg.Comments;
+            }
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            if (_reg != null && _registrationDAO!=null) { 
+                _reg.Comments = txtComment.Text;
+                _registrationDAO.UpdateRegistration(_reg);
+                detailPanel.Visibility = Visibility.Collapsed;    
+            }
+            LoadRegistrations();
+        }
+
+        private void RegistrationsDataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (RegistrationsDataGrid.SelectedItem != null)
+            {
+                _reg = (Registration)RegistrationsDataGrid.SelectedItem;
+            }
+            detailPanel.Visibility = Visibility.Collapsed;
         }
     }
 }
